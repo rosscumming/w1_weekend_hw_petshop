b6aa6087a8c30429356a9363b0264bb654c2c9d8
Ross <rosso5792@gmail.com>
2019-11-22 15:29:25 +0000

    tenth test successfully complete

--- w1_weekend_hw_petshop/pet_shop.rb
+++ w1_weekend_hw_petshop/pet_shop.rb
@@ -1,11 +1,31 @@
-def pet_shop_name(name)
-  return name[:name]
+def pet_shop_name(store_name)
+  return store_name[:name]
 end
 
-def total_cash(sum)
-  sum[:admin][:total_cash]
+def total_cash(store_total_cash)
+  return store_total_cash[:admin][:total_cash]
 end
 
 def add_or_remove_cash(pet_shop, money_added)
   pet_shop[:admin][:total_cash] += money_added
+end
+
+def pets_sold(pet_shop)
+  return pet_shop[:admin][:pets_sold]
+end
+
+def increase_pets_sold(pet_shop, pets_sold)
+  return pet_shop[:admin][:pets_sold] += pets_sold
+end
+
+def stock_count(pet_shop)
+  return pet_shop[:pets].size
+end
+
+def pets_by_breed(pet_shop, breed_type)
+  breed_total = []
+  for each_pet in pet_shop[:pets]
+    breed_total << each_pet if each_pet[:breed] == breed_type
+  end
+return breed_total
 end
--- w1_weekend_hw_petshop/specs/pet_shop_spec.rb
+++ w1_weekend_hw_petshop/specs/pet_shop_spec.rb
@@ -102,36 +102,36 @@ class TestPetShop < Minitest::Test
     assert_equal(990, cash)
   end
 
-  # def test_pets_sold
-  #   sold = pets_sold(@pet_shop)
-  #   assert_equal(0, sold)
-  # end
+  def test_pets_sold
+    sold = pets_sold(@pet_shop)
+    assert_equal(0, sold)
+  end
 
-  # def test_increase_pets_sold
-  #   increase_pets_sold(@pet_shop,2)
-  #   sold = pets_sold(@pet_shop)
-  #   assert_equal(2, sold)
-  # end
+  def test_increase_pets_sold
+    increase_pets_sold(@pet_shop,2)
+    sold = pets_sold(@pet_shop)
+    assert_equal(2, sold)
+  end
 
-  # def test_stock_count
-  #   count = stock_count(@pet_shop)
-  #   assert_equal(6, count)
-  # end
+  def test_stock_count
+    count = stock_count(@pet_shop)
+    assert_equal(6, count)
+  end
 
-  # def test_all_pets_by_breed__found
-  #   pets = pets_by_breed(@pet_shop, "British Shorthair")
-  #   assert_equal(2, pets.count)
-  # end
+  def test_all_pets_by_breed__found
+    pets = pets_by_breed(@pet_shop, "British Shorthair")
+    assert_equal(2, pets.count)
+  end
 
-  # def test_all_pets_by_breed__not_found
-  #   pets = pets_by_breed(@pet_shop, "Dalmation")
-  #   assert_equal(0, pets.count)
-  # end
+  def test_all_pets_by_breed__not_found
+    pets = pets_by_breed(@pet_shop, "Dalmation")
+    assert_equal(0, pets.count)
+  end
 
-  # def test_find_pet_by_name__returns_pet
-  #   pet = find_pet_by_name(@pet_shop, "Arthur")
-  #   assert_equal("Arthur", pet[:name])
-  # end
+  def test_find_pet_by_name__returns_pet
+    pet = find_pet_by_name(@pet_shop, "Arthur")
+    assert_equal("Arthur", pet[:name])
+  end
 
   # def test_find_pet_by_name__returns_nil
   #   pet = find_pet_by_name(@pet_shop, "Fred")
